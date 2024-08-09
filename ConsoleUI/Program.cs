6b37fdd9179b6b203c3c29bde354bda80e490323
Parsa <mehdipourparsa@gmail.com>
2024-08-09 14:46:52 -0400

    feat : add client code

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using DemoLibrary;
+using DemoLibrary.ConcreteBuilders;
+using DemoLibrary.ProductClasses;
+
+PizzaDirector director = new PizzaDirector();
+
+PizzaBuilder pizzaBuilder = new PizzaBuilder();
+PizzaRecipeBuilder recipeBuilder = new PizzaRecipeBuilder();
+
+// Create Margherita pizza
+director.CreateMargherita(pizzaBuilder);
+Pizza margherita = pizzaBuilder.GetProduct();
+
+director.CreateMargherita(recipeBuilder);
+PizzaRecipe margheritaRecipe = recipeBuilder.GetProduct();
+
+margherita.Display();
+Console.WriteLine();
+margheritaRecipe.DisplayRecipe();
+
+Console.WriteLine("\n" + new string('-', 50) + "\n");
+
+// Create Pepperoni pizza
+director.CreatePepperoni(pizzaBuilder);
+Pizza pepperoni = pizzaBuilder.GetProduct();
+
+director.CreatePepperoni(recipeBuilder);
+PizzaRecipe pepperoniRecipe = recipeBuilder.GetProduct();
+
+pepperoni.Display();
+Console.WriteLine();
+pepperoniRecipe.DisplayRecipe();
